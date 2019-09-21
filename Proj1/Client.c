1ac5ad031c0ccf2679eaa882ca01db8d4b077399
Gabriel Almeida <galmeida9@gmail.com>
2019-09-21 19:39:10 +0100

    almost done with client registring user

--- Proj1/Client.c
+++ Proj1/Client.c
@@ -9,6 +9,8 @@
 #include <string.h>
 
 #define  DEFAULT_PORT "58013"
+#define BUFFER_SIZE 128
+#define ID_SIZE 5
 
 void parseArgs(int number, char** arguments, char **port, char **ip);
 void connectToServer(int *udp_fd, int *tcp_fd, char *ip, char *port, struct addrinfo hints, struct addrinfo **resUDP, struct addrinfo **resTCP);
@@ -16,6 +18,8 @@ void SendMessageUDP(char *message, int fd, struct addrinfo *res);
 void receiveMessageUDP(int fd, socklen_t addrlen, struct sockaddr_in addr);
 void SendMessageTCP(char *message, int fd, struct addrinfo *res);
 void receiveMessageTCP(int fd);
+void parseCommands(int *userId, int udp_fd, int tcp_fd, struct addrinfo *resUDP, struct addrinfo *resTCP);
+void registerNewUser(int id, int fd, struct addrinfo *res);
 
 int main(int argc, char** argv) {
     int *udp_fd = malloc(sizeof(int));
@@ -34,6 +38,14 @@ int main(int argc, char** argv) {
 
     *udp_fd = -1; *tcp_fd = -1;
     connectToServer(udp_fd, tcp_fd, ip, port, hints, &resUDP, &resTCP);
+
+    char msg[] = "mensagem muita grande para ver se dá para enviar os tópicos todos ou não, para ver se está tudo fixe ou quê, um gajo tem de duvidar de tudo\n",
+        msg2[] = "ABC 1",
+        msg3[] = "REG 54321";
+    /*SendMessageUDP(msg3, *udp_fd, resUDP);*/
+
+    int userId;
+    parseCommands(&userId, *udp_fd, *tcp_fd, resUDP, resTCP);
 
     return 0;
 }
@@ -124,4 +136,35 @@ void receiveMessageTCP(int fd) {
     n = read(fd, buffer, 128);
     if (n == -1) exit(1);
     printf("%s\n", buffer);
+}
+
+void parseCommands(int *userId, int udp_fd, int tcp_fd, struct addrinfo *resUDP, struct addrinfo *resTCP) {
+    char *line = malloc(sizeof(char) * BUFFER_SIZE), *command;
+    size_t size;
+
+    while(1) {
+        getline(&line, &size, stdin);
+        command = strtok(line, " ");
+
+        if (strcmp(command, "register") == 0 || strcmp(command, "reg") == 0) {
+            command = strtok(NULL, " ");
+            if (command != NULL && strlen(command) == ID_SIZE + 1) {
+                *userId = atoi(strtok(command, "\n"));
+                strtok(NULL, " ") != NULL ? printf("Invalid command.\n") : registerNewUser(*userId, udp_fd, resUDP);
+                
+            }
+            else {
+                printf("Invalid command.\n");
+            }
+        }
+        else if (strcmp(line, "exit\n") == 0) {
+            return;
+        }
+    }
+}
+
+void registerNewUser(int id, int fd, struct addrinfo *res) {
+    printf("New User: %d\n", id);
+    SendMessageUDP("REG " + id, fd, res);
+    receiveMessageTCP(fd);
 }
