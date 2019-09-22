85f6ef4474acb951df84d6af72ccbe41b380c3d0
pgalhardo <pgalhardo@me.com>
2019-09-22 19:21:48 +0100

    Client topic_list

---
+++ Proj1/Client.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <assert.h>
+
+#define  DEFAULT_PORT "58013"
+#define BUFFER_SIZE 128
+#define ID_SIZE 5
+#define REGISTER_SIZE 12
+
+void parseArgs(int number, char** arguments, char **port, char **ip);
+void connectToServer(int *udp_fd, int *tcp_fd, char *ip, char *port, struct addrinfo hints, struct addrinfo **resUDP, struct addrinfo **resTCP);
+void SendMessageUDP(char *message, int fd, struct addrinfo *res);
+char* receiveMessageUDP(int fd, socklen_t addrlen, struct sockaddr_in addr);
+void SendMessageTCP(char *message, int fd, struct addrinfo *res);
+char* receiveMessageTCP(int fd);
+void parseCommands(int *userId, int udp_fd, int tcp_fd, struct addrinfo *resUDP, struct addrinfo *resTCP, socklen_t addrlen, struct sockaddr_in addr);
+void registerNewUser(int id, int fd, struct addrinfo *res, socklen_t addrlen, struct sockaddr_in addr);
+void requestLTP(int fd, struct addrinfo *res, socklen_t addrlen, struct sockaddr_in addr);
+
+int main(int argc, char** argv) {
+    int *udp_fd = malloc(sizeof(int));
+    int *tcp_fd = malloc(sizeof(int));
+    char* ip, *port;
+    ssize_t n;
+    socklen_t addrlen;
+    struct addrinfo hints, *resUDP, *resTCP;
+    struct sockaddr_in addr;
+    char buffer[128];
+
+    port = DEFAULT_PORT;
+    ip = "127.0.0.1";
+    parseArgs(argc, argv, &port, &ip);
+    printf("ip: %s\nport: %s\n", ip, port);
+
+    *udp_fd = -1; *tcp_fd = -1;
+    connectToServer(udp_fd, tcp_fd, ip, port, hints, &resUDP, &resTCP);
+
+    int userId;
+    parseCommands(&userId, *udp_fd, *tcp_fd, resUDP, resTCP, addrlen, addr);
+
+    freeaddrinfo(resTCP);
+    freeaddrinfo(resUDP);
+    close(*udp_fd);
+    close(*tcp_fd);
+    free(udp_fd);
+    free(tcp_fd);
+    return 0;
+}
+
+void parseArgs(int number, char** arguments, char **port, char **ip) {
+    int opt; 
+       
+    while((opt = getopt(number, arguments, "n:p:")) != -1) {  
+        switch(opt) {   
+            case 'n':  
+                if (optarg == NULL || strcmp(optarg, "-p") == 0) {
+                    printf("The IP is missing.\n");
+                    exit(1);
+                }
+                *ip = strdup(optarg);
+                break;  
+            case 'p':  
+                if (optarg == NULL || strcmp(optarg, "-n") == 0) {
+                    printf("The port is missing.\n");
+                    exit(1);
+                }
+                *port = strdup(optarg);
+                break;  
+        }  
+    }
+}
+
+void connectToServer(int *udp_fd, int *tcp_fd, char *ip, char *port, struct addrinfo hints, struct addrinfo **resUDP, struct addrinfo **resTCP) {
+    ssize_t n;
+    memset(&hints,0, sizeof(hints));
+    hints.ai_family=AF_INET;
+    hints.ai_flags=AI_NUMERICSERV;
+
+    /*UDP Connection */
+    if (*udp_fd < 0) {
+        hints.ai_socktype=SOCK_DGRAM;
+        n = getaddrinfo(ip, port, &hints, resUDP);
+        if (n != 0) exit(1);
+
+        *udp_fd = socket((*resUDP)->ai_family, (*resUDP)->ai_socktype, (*resUDP)->ai_protocol);
+        if (*udp_fd == -1) exit(1);
+    }
+
+    /*TCP Connection */
+    if (*tcp_fd < 0){
+        hints.ai_socktype=SOCK_STREAM;
+        n = getaddrinfo(ip, port, &hints, resTCP);
+        if (n != 0) exit(1);
+
+        *tcp_fd = socket((*resTCP)->ai_family, (*resTCP)->ai_socktype, (*resTCP)->ai_protocol);
+        if (*tcp_fd == -1) exit(1);
+    }
+}
+
+void SendMessageUDP(char *message, int fd, struct addrinfo *res) {
+    ssize_t n;
+
+    n = sendto(fd, message, strlen(message), 0, res->ai_addr, res->ai_addrlen);
+    if (n == -1) exit(1);
+    printf("enviei %s", message);
+}
+
+char* receiveMessageUDP(int fd, socklen_t addrlen, struct sockaddr_in addr) {
+    ssize_t n;  
+    char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
+    addrlen = sizeof(addr);
+
+    while (buffer[(strlen(buffer) - 1)] != '\n') {
+        n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*) &addr, &addrlen);
+        if (n == -1) exit(1);
+    }
+    
+    printf("%s\n", buffer);
+    return buffer;
+}
+
+void SendMessageTCP(char *message, int fd, struct addrinfo *res) {
+    ssize_t n;
+
+    n = connect(fd, res->ai_addr, res->ai_addrlen);
+    if (n == -1) exit(1);
+
+    n = write(fd, message, strlen(message));
+    if (n == -1) exit(1);
+    printf("enviei\n");
+}
+
+char* receiveMessageTCP(int fd) {
+    ssize_t n;  
+    char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
+
+    n = read(fd, buffer, 128);
+    if (n == -1) exit(1);
+    printf("%s\n", buffer);
+
+    return buffer;
+}
+
+void parseCommands(int *userId, int udp_fd, int tcp_fd, struct addrinfo *resUDP, struct addrinfo *resTCP, socklen_t addrlen, struct sockaddr_in addr) {
+    char *line = malloc(sizeof(char) * BUFFER_SIZE), *command;
+    size_t size;
+
+    while(1) {
+        getline(&line, &size, stdin);
+        command = strtok(line, " ");
+
+        if (strcmp(command, "register") == 0 || strcmp(command, "reg") == 0) {
+            command = strtok(NULL, " ");
+            if (command != NULL && strlen(command) == ID_SIZE + 1) {
+                *userId = atoi(strtok(command, "\n"));
+                strtok(NULL, " ") != NULL  || *userId == 0? printf("Invalid command.\n") : registerNewUser(*userId, udp_fd, resUDP, addrlen, addr);
+                
+            }
+            else {
+                printf("Invalid command.\n");
+            }
+        }
+
+        else if (strcmp(command, "topic_list\n") == 0 || strcmp(command, "tl\n") == 0)
+            requestLTP(udp_fd, resUDP, addrlen, addr);
+
+        else if (strcmp(line, "exit\n") == 0) {
+            free(line);
+            return;
+        }
+
+        else printf("Invalid command.\n");
+    }
+}
+
+void registerNewUser(int id, int fd, struct addrinfo *res, socklen_t addrlen, struct sockaddr_in addr) {
+    char *message = malloc(sizeof(char) * REGISTER_SIZE);
+
+    snprintf(message, REGISTER_SIZE, "REG %d\n", id);
+    SendMessageUDP(message, fd, res);
+    char* status = receiveMessageUDP(fd, addrlen, addr);
+    strcmp(status, "OK\n") ==  0 ? printf("Registration Complete!\n") : printf("Could not register user, invalid user ID.\n");
+    free(message);
+    free(status);
+}
+
+void requestLTP(int fd, struct addrinfo *res, socklen_t addrlen, struct sockaddr_in addr) {
+    int i = 1, N;
+    char * iter, * ltr;
+
+    SendMessageUDP("LTP\n", fd, res);
+    ltr = receiveMessageUDP(fd, addrlen, addr);
+
+    assert(!strcmp(strtok(ltr, " "), "LTR"));
+    N = atoi(strtok(NULL, " "));
+
+    while (i <= N) {
+        iter = strtok(NULL, " ");
+        printf("%d: %s\n", i++, iter);
+    }
+
+    free(ltr);
+}
---
+++ Proj1/Server.c
@@ -0,0 +1,236 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define PORT "58013"
+#define BUFFER_SIZE 128
+#define ID_SIZE 5
+
+int nUDP, nTCP, fdUDP, fdTCP, newfd;
+socklen_t addrlenUDP, addrlenTCP;
+struct addrinfo hintsUDP, hintsTCP, *resUDP, *resTCP;
+struct sockaddr_in addrUDP, addrTCP;
+char buffer[BUFFER_SIZE];
+
+char* processUDPMessage(char* buffer, int len);
+int checkIfStudentCanRegister(int number);
+char* registerNewStudent(char* arg1);
+void handleKill(int sig);
+
+int main(int argc, char** argv){
+    char port[6];
+    struct sigaction handle_kill;
+    sigset_t int_set;
+    sigemptyset(&int_set);
+    sigaddset(&int_set, SIGINT);
+    handle_kill.sa_handler = handleKill;
+    handle_kill.sa_flags = SA_RESTART;
+    sigemptyset(&handle_kill.sa_mask);
+    sigaction(SIGINT, &handle_kill, NULL);
+
+    strcpy(port, PORT);
+
+    printf("Welcome to RC Forum\n");
+
+    /*Get port from arguments*/
+    int opt; 
+
+    if (argc == 2){
+        printf("The port is missing.\n");
+        exit(1);
+    }
+       
+    while((opt = getopt(argc, argv, "p:")) != -1) {  
+        switch(opt) {   
+            case 'p':
+                strcpy(port, optarg);
+                break;
+        }
+    }
+
+    printf("Port: %s\n", port);
+
+    /*UDP Server*/
+    memset(&hintsUDP,0, sizeof(hintsUDP));
+    hintsUDP.ai_family=AF_INET;
+    hintsUDP.ai_socktype=SOCK_DGRAM;
+    hintsUDP.ai_flags=AI_PASSIVE|AI_NUMERICSERV;
+    
+    nUDP = getaddrinfo(NULL, port, &hintsUDP, &resUDP);
+    if (nUDP !=0 ) /*error*/ exit(1);
+
+    fdUDP = socket(resUDP->ai_family, resUDP->ai_socktype, resUDP->ai_protocol);
+    if (fdUDP == -1) /*error*/ exit(1);
+
+    nUDP = bind(fdUDP, resUDP->ai_addr, resUDP->ai_addrlen);
+    if (nUDP == -1) /*error*/ exit(1);
+    
+    printf("Created UDP Server\n");
+    
+
+    /*TCP Server*/
+    memset(&hintsTCP, 0, sizeof(hintsTCP));
+    hintsTCP.ai_family = AF_INET;
+    hintsTCP.ai_socktype = SOCK_STREAM;
+    hintsTCP.ai_flags = AI_PASSIVE|AI_NUMERICSERV;
+
+    nTCP = getaddrinfo(NULL, port, &hintsTCP, &resTCP);
+    if (nTCP != 0) exit(1);
+
+    fdTCP = socket(resTCP->ai_family, resTCP->ai_socktype, resTCP->ai_protocol);
+    if (fdTCP == -1) exit(1);
+
+    nTCP = bind(fdTCP, resTCP->ai_addr, resTCP->ai_addrlen);
+    if (nTCP == -1) exit(1);
+
+    if (listen(fdTCP, 5) == -1) exit(1);
+
+    printf("Created TCP Server\n");
+    
+    /*Wait for communication*/
+    int maxFd;
+    fd_set readset;
+
+    FD_ZERO(&readset);
+    maxFd = fdUDP > fdTCP ? fdUDP : fdTCP;
+
+    printf("\n");
+    while (1) {
+        int result;
+        ssize_t nMsg;
+
+        /*Setup fd in readset*/
+        FD_SET(fdUDP, &readset);
+        FD_SET(fdTCP, &readset);
+        
+        /*Select the result selector*/
+        result = select(maxFd+1, &readset, NULL, NULL, NULL);
+        if (result == -1) continue;
+        else {
+            if (FD_ISSET(fdUDP, &readset)){
+                printf("\nUDP\n");
+                int addrlen = sizeof(addrUDP);
+
+                nMsg = recvfrom(fdUDP, buffer, BUFFER_SIZE, 0, (struct sockaddr*) &addrUDP, &addrlen);
+                if (nMsg == -1) /*error*/ exit(1);
+
+                /*Analyze message*/
+                char *response = processUDPMessage(buffer, BUFFER_SIZE);
+                //write(1, "received: ", 10); write(1, buffer, nMsg);
+
+                /*Send response*/
+                nMsg = sendto(fdUDP, response, strlen(response), 0, (struct sockaddr*) &addrUDP, addrlen);
+                if (nMsg == -1) /*error*/ exit(1);
+
+            }
+            else if (FD_ISSET(fdTCP, &readset)){
+                printf("\nTCP\n");
+
+                if ((newfd = accept(fdTCP, (struct sockaddr*) &addrTCP, &addrlenTCP)) == -1) exit(1);
+
+                /*Analyze message*/
+                nMsg = read(newfd, buffer, BUFFER_SIZE);
+                if (nMsg == -1) exit(1);
+
+                //write(1, "received: ", 10); write(1, buffer, nMsg);
+
+                /*Send response*/
+                nMsg = write(newfd, buffer, nMsg);
+                if (nMsg == -1) exit(1);
+            }
+        }
+    }
+
+    freeaddrinfo(resUDP);
+    freeaddrinfo(resTCP);
+    close(fdUDP);
+    close(fdTCP);
+}
+
+char* processUDPMessage(char* buffer, int len){
+    const char s[2] = " ";
+    char command[4] = "NUL", *arg1 = NULL, *response;
+
+    char *token;
+    int i = 0;
+    token = strtok(buffer, s);
+    while (token != NULL){
+        if (i==0) strcpy(command, token);
+        else {
+            int lenArg = strlen(token);
+            arg1 = strdup(token);
+        }
+        token = strtok(NULL, s);
+        i++;
+    }
+
+    printf("Command: %s, Arg: %s\n", command, arg1);
+    
+    if (!strcmp(command, "REG")){
+        response = registerNewStudent(arg1);
+        return response;
+    }
+
+    else if (!strcmp(command, "LTP\n")){
+        //TODO: remove default msg
+        //      check format
+        return "LTR 2 rc:89522 cg:12345\n";
+    }
+
+    else {
+        printf("Command not found.\n");
+    }
+}
+
+int checkIfStudentCanRegister(int number){
+    FILE* fp = fopen("students.txt", "r");
+    int currNumber = -1;
+    char line[6] = "";
+    while (fgets(line, sizeof(line), fp)){
+        currNumber = atoi(line);
+        if (currNumber == number) return 1;
+    }
+    fclose(fp);
+    return 0;
+}
+
+char* registerNewStudent(char* arg1){
+    char *response;
+    int stuNumber = atoi(arg1);
+
+    if (stuNumber == 0) {
+        printf("Number error.\n");
+        response = strdup("NOK\n");
+        return response;
+    }
+
+    int enabled = checkIfStudentCanRegister(stuNumber);
+    if (!enabled) {
+        printf("Register %d: refused.\n", stuNumber);    
+        response = strdup("NOK\n");
+        return response;
+    }
+
+    printf("Register %d: accepted.\n", stuNumber);
+
+    /*TODO: Register on file??*/
+
+    response = strdup("OK\n");
+    return response;
+}
+
+void handleKill(int sig){
+    freeaddrinfo(resUDP);
+    freeaddrinfo(resTCP);
+    close(fdUDP);
+    close(fdTCP);
+    _Exit(EXIT_SUCCESS);
+}
