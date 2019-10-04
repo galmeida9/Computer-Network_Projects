1d7f4b691ea1f9f4430b8dbcb0edda8c4159bed4
pgalhardo <pgalhardo@me.com>
2019-10-04 17:07:48 +0100

    Fix some memory issues while copying strings

--- Proj1/Server.c
+++ Proj1/Server.c
@@ -249,7 +249,8 @@ char* processTCPMessage(char* buffer, int len, int fd){
 
     //bufferBackup = strdup(buffer);
     memcpy(bufferBackup, buffer, len);
-
+    bufferBackup[len] = '\0';
+    
     command = strtok(buffer, " ");
 
     if (!strcmp(command, "GQU"))
@@ -473,11 +474,9 @@ void freeTopicInList() {
 
 char* questionGet(char *input, int fd) {
     strtok(input, " ");
-
     char *response;
     char *topic = strtok(NULL, " ");
-    char *question = strtok(NULL, " ");
-    char *leftover = strtok(NULL, " ");
+    char *question = strtok(NULL, "\n");
 
     if (topic == NULL) {
         response = strdup("QGR ERR\n");
@@ -489,11 +488,6 @@ char* questionGet(char *input, int fd) {
     }
 
     if (question == NULL) {
-        response = strdup("QGR ERR\n");
-        return response;
-    }
-
-    if (leftover != NULL) {
         response = strdup("QGR ERR\n");
         return response;
     }
@@ -521,6 +515,7 @@ char* questionGet(char *input, int fd) {
 
     while ((nread = getline(&line, &len, questionsFd)) != -1) {
         char *token = strtok(line, ":");
+
         if (strcmp(token, question) == 0) {
             foundQuestion = 1;
             qUserId = atoi(strtok(NULL, ":"));
